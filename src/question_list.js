c9e0d40c2470cd4577ea4dd695fbe321d0b155ff
Tom Buckley <tbuckley89@gmail.com>
2015-11-08 23:03:21 -0500

    Removed fake page

--- src/question_list.js
+++ src/question_list.js
@@ -1,5 +1,4 @@
 module.exports = [
 	{code: "1", title: "Declarations", component: require('./questions/01_declaration')},
 	{code: "2", title: "Services", component: require('./questions/02_services')},
-	{code: "3", title: "Foo", component: require('./questions/02_services')},
 ];
