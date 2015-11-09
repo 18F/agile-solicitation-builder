ecbfce9fd7b42eef1a46301e7ce5a6bad88811fa
Tom Buckley <tbuckley89@gmail.com>
2015-11-09 08:57:17 -0800

    Added error notifications to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,16 @@ var reactify = require('reactify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var rename = require('gulp-rename');
+var notify = require("gulp-notify");
+
+function handleErrors() {
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: "Compile Error",
+    message: "<%= error.message %>"
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
 
 gulp.task('default', function() {
     var bundler = browserify({
@@ -21,12 +31,14 @@ gulp.task('default', function() {
 	        console.log('Updating!');
 
 	        watcher.bundle() // Create new bundle that uses the cache for high performance
+	        	.on('error', handleErrors)
 	        	.pipe(source('./src/app.js'))
 		    	.pipe(rename('bundle.js'))
 		    	.pipe(gulp.dest('./build'));
 	        console.log('Updated!', (Date.now() - updateStart) + 'ms');
 	    })
 	    .bundle() // Create the initial bundle when starting the task
+	    .on('error', handleErrors)
 	    .pipe(source('./src/app.js'))
     	.pipe(rename('bundle.js'))
     	.pipe(gulp.dest('./build'));
