f5b9244c2dd541505435f2843068e956c176a1d7
Tom Buckley <tbuckley89@gmail.com>
2015-11-08 22:23:55 -0800

    Moved button logic out of individual questions

--- src/question.js
+++ src/question.js
@@ -1,29 +1,84 @@
 var React = require('react');
 
+// Bootstrap
+var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
+var Button = require('react-bootstrap').Button;
+
+// Router stuff
+var Link = require('react-router').Link;
+var History = require('react-router').History;
+
 // Custom components
 var questionList = require('./question_list');
 
 var Question = React.createClass({
-	render: function() {
-		// Store the question to display in `question`.
-		var question;
-		questionList.forEach(function(q) {
-			if(q.code == this.props.params.qid) {
-				var Component = q.component;
-				question = (
-					<Component params={this.props.params} />
-				);
+	mixins: [History],
+
+	linkForOffset: function(offset) {
+		var currentIndex;
+		questionList.forEach(function(question, i) {
+			if(question.code == this.props.params.qid) {
+				currentIndex = i;
 			}
 		}.bind(this));
-		
-		// If there was no match
-		if(!question) {
-			question = "Unknown question: '"+this.props.params.qid+"'";
-		}
 
-		return (
-			<div>{question}</div>
-		);
+		var baseURL = "/rfp/"+this.props.params.id;
+		var nextIndex = currentIndex + offset;
+		if(nextIndex < 0) {
+			return baseURL;
+		} else if(nextIndex >= questionList.length) {
+			return baseURL + "/results";
+		} else {
+			return baseURL + "/question/" + questionList[nextIndex].code;
+		}
+	},
+	handlePrev: function() {
+		this.refs.question.save(function() {
+			var prevLink = this.linkForOffset(-1);
+			this.history.pushState(null, prevLink, null);
+		}.bind(this));
+	},
+	handleNext: function() {
+		this.refs.question.save(function() {
+			var nextLink = this.linkForOffset(1);
+			this.history.pushState(null, nextLink, null);
+		}.bind(this));
+	},
+	getComponentForQuestionID: function(qid) {
+		for(var i = 0; i < questionList.length; i++) {
+			var question = questionList[i];
+			if(question.code == qid) {
+				return question.component;
+			}
+		}
+		return null;
+	},
+
+	getInitialState: function() {
+		return {saving: false};
+	},
+	render: function() {
+		var Component = this.getComponentForQuestionID(this.props.params.qid);
+
+		if(Component) {
+			return (
+				<div>
+					<div>
+						<Component params={this.props.params} ref="question" />
+					</div>
+					<ButtonToolbar>
+						<Button onClick={this.handlePrev} disabled={this.state.saving}>Previous</Button>
+						<Button bsStyle="primary" onClick={this.handleNext} disabled={this.state.saving}>Next</Button>
+					</ButtonToolbar>
+				</div>
+			);
+		} else {
+			return (
+				<div>
+					<div>{"Unknown question: '"+this.props.params.qid+"'"}</div>
+				</div>
+			);
+		}
 	},
 });
 
--- src/questions/01_declaration.js
+++ src/questions/01_declaration.js
@@ -1,43 +1,14 @@
 var React = require('react');
 
-// Bootstrap
-var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
-var Button = require('react-bootstrap').Button;
-
-// Router stuff
-var Link = require('react-router').Link;
-var History = require('react-router').History;
-
 var Declaration = React.createClass({
-	mixins: [History],
 	save: function(cb) {
 		// TODO: save data
-		this.setState({saving: true});
 		setTimeout(cb, 500);
-	},
-	handlePrev: function() {
-		this.save(function() {
-			var prevLink = "/rfp/"+this.props.params.id;
-			this.history.pushState(null, prevLink, null);
-		}.bind(this));
-	},
-	handleNext: function() {
-		this.save(function() {
-			var nextLink = "/rfp/"+this.props.params.id+"/question/2";
-			this.history.pushState(null, nextLink, null);
-		}.bind(this));
-	},
-	getInitialState: function() {
-		return {saving: false};
 	},
 	render: function() {
 		return (
 			<div>
 				<div>Declaration questions...</div>
-				<ButtonToolbar>
-					<Button onClick={this.handlePrev} disabled={this.state.saving}>Previous</Button>
-					<Button bsStyle="primary" onClick={this.handleNext} disabled={this.state.saving}>Next</Button>
-				</ButtonToolbar>
 			</div>
 		);
 	},
