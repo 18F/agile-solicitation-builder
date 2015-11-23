06d4c36b0454e2681aa7ecf3b40b45571af230ae
Tom Buckley <tbuckley89@gmail.com>
2015-11-23 15:55:36 -0500

    Disable continue button until all responses are recorded

--- src/request_overview.js
+++ src/request_overview.js
@@ -7,37 +7,68 @@ var Button = require('react-bootstrap').Button;
 var Link = require('react-router').Link;
 
 
+var AGENCY_NAMES = {
+	"ED": "Department of Education",
+	"DOE": "Department of Energy",
+	"EPA": "Environmental Protection Agency",
+	"GSA": "General Services Administration",
+	"DOL": "Department of Labor",
+	"NARA": "National Archives and Records Administration",
+	"NASA": "National Aeronautics and Space Administration",
+	"OMB": "Office of Management and Budget",
+	"VA": "Department of Veteran Affairs"
+};
+
+var DOC_TYPES = {
+	contact: "a new purchase under Far 15 (Contract)",
+	purchaseOrder: "a new purchase under Far 13 (Purchase Order)",
+	taskOrder: "being issued off an existing Indefinite Delivery Indefinite Quantity (ID/IQ) (Task Order)",
+	call: "being ordered off an existing Blanket Purchase Agreement (BPA) (Call)",
+};
+
 var RequestOverview = React.createClass({
 	getInitialState: function() {
 		return {
 			docType: localStorage.getItem("docType"),
-			agency: localStorage.getItem("agency"),
+			agency: localStorage.getItem("agency") || "none",
 		};
 	},
 	setAgency: function(event) {
-		var agencyFullNames = {
-				"ED": "Department of Education",
-				"DOE": "Department of Energy",
-				"EPA": "Environmental Protection Agency",
-				"GSA": "General Services Administration",
-				"DOL": "Department of Labor",
-				"NARA": "National Archives and Records Administration",
-				"NASA": "National Aeronautics and Space Administration",
-				"OMB": "Office of Management and Budget",
-				"VA": "Department of Veteran Affairs"
-		}
-		var response = event.target.value;
-		if (response.length > 1){
-			localStorage.agency = response;
-			localStorage.agencyFullname = "U.S. " + agencyFullNames[response];
-			this.setState({agency: response});
-		}
+		// localStorage.agencyFullname = "U.S. " + AGENCY_NAMES[response];
+		localStorage.setItem("agency", event.target.value);
+		this.setState({agency: event.target.value});
 	},
 	setDoctype: function(event) {
-		localStorage.docType = event.target.value;
+		localStorage.setItem("docType", event.target.value);
 		this.setState({docType: event.target.value});
 	},
 	render: function() {
+		// Create the agency names list
+		var agencyNameOptions = [(
+			<option value="none">-- Please select --</option>
+		)];
+		for(var key in AGENCY_NAMES) {
+			agencyNameOptions.push(
+				<option value={key}>{AGENCY_NAMES[key]} ({key})</option>
+			);
+		}
+
+		// Create the doc type radio list
+		var docTypeOptions = [];
+		for(var key in DOC_TYPES) {
+			docTypeOptions.push(
+				<div className="radio">
+					<label>
+						<input type="radio" value={key} checked={key == this.state.docType} />... {DOC_TYPES[key]}
+				  </label>
+				</div>
+			);
+		}
+
+		var validAgency = this.state.agency != null && this.state.agency != "none";
+		var validDocType = this.state.docType != null;
+		var continueDisabled = !(validAgency && validDocType);
+
 		return (
 			<div>
 				<div>
@@ -46,53 +77,23 @@ var RequestOverview = React.createClass({
 					<p>We'll ask you some questions to understand what you want to build,
 					and then let you download the generated documents.</p>
 					
-					
 					<h5>Firstly, what agency is this for?</h5>
-						<select className="form-control medium-response" onChange={this.setAgency} value={this.state.agency}>
-							<option value=""></option>
-							<option value="ED" >Department of Education (ED)</option>
-							<option value="DOE" >Department of Energy (DOE)</option>
-						  <option value="EPA" >Environmental Protection Agency (EPA)</option>
-						  <option value="GSA" >General Services Administration (GSA)</option>
-						  <option value="DOL" >Department of Labor (DOL)</option>
-						  <option value="NARA" >National Archives and Records Administration (NARA)</option>
-						  <option value="NASA" >National Aeronautics and Space Administration (NASA)</option>
-						  <option value="OMB" >Office of Management and Budget (OMB)</option>
-						  <option value="VA" >Department of Veteran Affairs (VA)</option>
-						</select>
+					<select className="form-control medium-response" onChange={this.setAgency} value={this.state.agency}>
+						{agencyNameOptions}
+					</select>
+
 					<br />
 
 					<h5>This will be ...</h5>
-
-					<div className="radio">
-					  <label>
-					    <input type="radio" value="Contract" onChange={this.setDoctype} checked={this.state.docType === "Contract"}></input>
-					    ... a new purchase under Far 15 (Contract)
-					  </label>
-					</div>
-					<div className="radio">
-					  <label>
-					    <input type="radio" value="Purchase Order" onChange={this.setDoctype} checked={this.state.docType === "Purchase Order"}></input>
-					    ... a new purchase under Far 13 (Purchase Order)
-					  </label>
-					</div>
-					<div className="radio">
-					  <label>
-					    <input type="radio" value="Task Order" onChange={this.setDoctype} checked={this.state.docType === "Task Order"}></input>
-					    ... being issued off an existing Indefinite Delivery Indefinite Quantity (ID/IQ) (Task Order)
-					  </label>
-					</div>
-					<div className="radio">
-					  <label>
-					    <input type="radio" value="Call" onChange={this.setDoctype} checked={this.state.docType === "Call"}></input>
-					    ... being ordered off an existing Blanket Purchase Agreement (BPA) (Call)
-					  </label>
-					</div>
+					<radiogroup onChange={this.setDoctype}>
+						{docTypeOptions}
+					</radiogroup>
 				</div>
 
 				<br />
+
 				<Link to={"/rfp/"+this.props.params.id+"/question/1"}>
-					<Button bsStyle="primary" >{"Let's go!"}</Button>
+					<Button bsStyle="primary" disabled={continueDisabled}>{"Let's go!"}</Button>
 				</Link>
 			</div>
 		);
