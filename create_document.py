1a1fffba73aaaa26b2dfee32ef4650c7cc7589ae
Julia Winn <unfinished.art@gmail.com>
2016-01-15 10:06:25 -0500

    syntax bug

--- create_document.py
+++ create_document.py
@@ -308,7 +308,7 @@ def inspection_and_delivery(document, rfq):
     document.add_heading("Collaboration Environment", level=SUB_HEADING)
 
     if cc["workspaceExists"] == "yes":
-        if cc["workspaceName"].length > 0:
+        if len(cc["workspaceName"]) > 0:
             document.add_paragraph(rfq.agency + " is currently using " + cc["workspaceName"]) + " as their primary collaborative workspace tool. The contractor is required to establish a collaborative workspace using either this tool or another that both the contractor and the CO can agree upon."
 
     document.add_paragraph(cc["deliveringDeliverables"])
