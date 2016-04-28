bd1feed60f2c5bc4c833998f0da37851d78214bd
Greg Walker <michael.walker@gsa.gov>
2016-04-28 08:46:03 -0500

    Add a route that just tells you whether the current user is logged in

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ import config
 import logging
 # import StringIO
 import io
+import base64
 
 from flask import Flask, send_from_directory, send_file, request, jsonify, g
 from flask_restful import Resource, Api, reqparse
@@ -345,6 +346,22 @@ def download(rfq_id):
 @auth.login_required
 def get_resource():
     return jsonify({ 'data': 'Hello, %s!' % g.user.username })
+
+@app.route('/api/isLoggedIn')
+def isLoggedIn():
+    auth = request.headers.get('Authorization')
+    result = { 'loggedIn': False }
+    # The Authorization header should look like "Basic username:password",
+    # so it must be at least 6 characters long or it's invalid.
+    if auth is not None and len(auth) > 6:
+        # Decode the username:password part.
+        pair = base64.b64decode(auth[6:])
+        # If the user is logged in, the username should be their token
+        # and the password should be "none".  Assume that the decoded
+        # string is ":none" and ditch it.  If that's not right, the
+        # verification will fail and that's a-okay.
+        result['loggedIn'] = verify_password(pair[:-5], '');
+    return jsonify(result)
 
 @app.route('/agile_estimator')
 def agile_estimator():
