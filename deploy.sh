90648103356ddb23ca3e92f545ec420bca61db66
Laura Gerhardt <laura.gerhardt@gsa.gov>
2016-05-19 14:02:07 -0400

    update to micropurchases travis deploy config

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+set -e
+
+API="https://api.cloud.gov"
+ORG="18f-acq"
+SPACE=$1
+
+if [ $# -ne 1 ]; then
+  echo "Usage: deploy <space>"
+  exit
+fi
+
+if [ $SPACE = 'production' ]; then
+  NAME="playbook-in-action"
+  MANIFEST="manifest.yml"
+elif [ $SPACE = 'staging' ]; then
+  NAME="playbook-in-action"
+  MANIFEST="manifest-staging.yml"
+else
+  echo "Unknown space: $SPACE"
+  exit
+fi
+
+cf login --a $API --u $CF_USERNAME --p $CF_PASSWORD --o $ORG -s $SPACE
+cf zero-downtime-push $NAME -f $MANIFEST
