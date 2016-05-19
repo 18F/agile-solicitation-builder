139a9f455716d75a566173f2325b37feafee006c
Laura Gerhardt <laura.gerhardt@gsa.gov>
2016-05-19 13:35:42 -0400

    update gulp, readme, to not need bower

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -15,34 +15,53 @@ function handleErrors() {
   this.emit('end'); // Keep gulp from hanging on this task
 }
 
-gulp.task('copy_jquery', function(){
-  gulp.source('bower_components/jquery/dist/jquery.min.js').pipe(ulp.dest('./build'));
+gulp.task('copyjquery', function(){
+  gulp.src('./node_modules/jquery/dist/jquery.min.js').pipe(gulp.dest('./build'));
 });
-gulp.task('default', function() {
-    var bundler = browserify({
+
+var bundler = browserify({
         entries: ['./src/app.js'], // Only need initial file, browserify finds the deps
         transform: [reactify], // We want to convert JSX to normal javascript
         debug: true, // Gives us sourcemapping
         cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
     });
 
-	var watcher  = watchify(bundler);
-
-    return watcher
-	    .on('update', function () { // When any files update
-	        var updateStart = Date.now();
-	        console.log('Updating!');
-
-	        watcher.bundle() // Create new bundle that uses the cache for high performance
-	        	.on('error', handleErrors)
-	        	.pipe(source('./src/app.js'))
-		    	.pipe(rename('bundle.js'))
-		    	.pipe(gulp.dest('./build'));
-	        console.log('Updated!', (Date.now() - updateStart) + 'ms');
-	    })
-	    .bundle() // Create the initial bundle when starting the task
-	    .on('error', handleErrors)
-	    .pipe(source('./src/app.js'))
-    	.pipe(rename('bundle.js'))
-    	.pipe(gulp.dest('./build'));
+gulp.task('bundling', function(){
+  bundler
+  .bundle()
+  .pipe(source('./src/app.js'))
+  .pipe(rename('bundle.js'))
+  .pipe(gulp.dest('./build'));
 });
+
+gulp.task('bundlingWatch', function () {
+  	var watcher  = watchify(bundler);
+
+      return watcher
+  	    .on('update', function () { // When any files update
+  	        var updateStart = Date.now();
+  	        console.log('Updating!');
+
+  	        watcher.bundle() // Create new bundle that uses the cache for high performance
+  	        	.on('error', handleErrors)
+  	        	.pipe(source('./src/app.js'))
+  		    	.pipe(rename('bundle.js'))
+  		    	.pipe(gulp.dest('./build'));
+  	        console.log('Updated!', (Date.now() - updateStart) + 'ms');
+  	    })
+  	    .bundle() // Create the initial bundle when starting the task
+  	    .on('error', handleErrors)
+  	    .pipe(source('./src/app.js'))
+        // .pipe(buffer())
+        // .pipe(uglify())
+      	.pipe(rename('bundle.js'))
+      	.pipe(gulp.dest('./build'));
+});
+
+gulp.task('default', ['bundlingWatch'], function() {
+  gulp.watch('./src/**/*.js', function(){
+    gulp.run('bundling');
+  });
+});
+
+gulp.task('build', ['bundling', 'copyjquery'], function(){});
