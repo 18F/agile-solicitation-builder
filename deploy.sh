491fb11e5062ed6d0b3c4fb07e1fd2095ef6bbd5
James C. Scott <jamesc.scott@gsa.gov>
2017-02-08 11:34:13 -0500

    GovCloud: change domains, creds & service names

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 set -e
 
-API="https://api.cloud.gov"
-ORG="18f-acq"
+API="https://api.fr.cloud.gov"
+ORG="gsa-acq-agile-solicitation-builder"
 SPACE=$1
 
 if [ $# -ne 1 ]; then
@@ -9,12 +9,16 @@ if [ $# -ne 1 ]; then
   exit
 fi
 
-if [ $SPACE = 'production' ]; then
+if [ $SPACE = 'prod' ]; then
   NAME="agile-solicitation-builder"
   MANIFEST="manifest.yml"
+  CF_USERNAME=$CF_USERNAME_PRODUCTION
+  CF_PASSWORD=$CF_PASSWORD_PRODUCTION
 elif [ $SPACE = 'staging' ]; then
   NAME="agile-solicitation-builder"
   MANIFEST="manifest-staging.yml"
+  CF_USERNAME=$CF_USERNAME_STAGING
+  CF_PASSWORD=$CF_PASSWORD_STAGING
 else
   echo "Unknown space: $SPACE"
   exit
